0d8879e48f7bb94d03fc024b79437c481220896f
KHANSADDAMKHAN <khansaddamkhan93@gmail.com>
2021-02-26 07:24:50 +0530

    Task 1 done.

--- DemoProject1/ViewController.swift
+++ DemoProject1/ViewController.swift
@@ -14,8 +14,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        chatStart()
     }
 
+    func chatStart() {
+        
+        print("Start chating!")
+
+    }
 
 }
 
